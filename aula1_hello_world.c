f1bba0a576d931e22c0fccf6e8bdff572fb934cd
wees-s <61247648+wees-s@users.noreply.github.com>
2025-02-28 12:28:31 -0300

    First commit - C study

---
+++ aula1_hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {//início da função
+    printf("Hello World!");//printf faz a print padrão no terminal.
+    return 0; //indica o fim da função
+}
---
+++ aula2_soma_basica.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    int b = 20;
+    printf("%d\n", a+b);
+    return 0;
+}
